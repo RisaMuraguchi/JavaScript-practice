f3eb7a61765c4654a1a214e8311d49f2f35b7e26
RisaMuraguchi <126639980+RisaMuraguchi@users.noreply.github.com>
2023-06-09 13:05:43 +0900

    練習

--- app.js
+++ app.js
@@ -1,3 +1,25 @@
 let hello = "Hello World";
 alert(hello);
 
+let user = {
+  name: '山田太郎',
+  age: 30,
+  birthday: '1991/7/21',
+  hobby: ['ゲーム','スポーツ'],
+  getAge: function () {
+    return this.age;
+  }
+}
+user.name = '田中次郎'
+console.log(user.name); // 田中次郎
+
+function foo() {
+  let x = "webcamp";
+  {
+    let y = "hello webcamp";
+  }
+  console.log(x);
+  console.log(y);
+}
+foo()
+
